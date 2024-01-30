dcc76618b2c6d243449e5d24b901e3095ed6f5f4
Amrutha D <86772253+am-ruth-a-d@users.noreply.github.com>
2024-01-30 11:29:22 +0900

    Add files via upload

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "VideochatApp": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://d7lol26brvc4nbhxvfchgfeeqe.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-knuin6j6dnc4dmzu57ucekpn2a"
+                }
+            }
+        }
+    }
+}''';
